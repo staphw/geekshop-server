64bb6d0bed05aea1ac8aa666b4d75b0f80000a3f
Serg <st.apostol@mail.ru>
2021-09-17 16:05:12 +0300

    lesson-2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from os.path import dirname
+import json
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'title': 'geekshop | main page'})
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = dict()
+    with open(dirname(__file__) + '/fixtures/products.json') as c:
+        for item in json.load(c):
+            context.update(item)
+    return render(request, 'mainapp/products.html', context)
