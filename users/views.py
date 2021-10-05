37427659b6c421b7e07a376c0c27735622ce18ee
staphw <st.apostol@mail.ru>
2021-10-05 12:59:55 +0300

    error message for templates

--- users/views.py
+++ users/views.py
@@ -16,8 +16,6 @@ def login(request):
             if user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-        else:
-            print(form.errors)
     else:
         form = UserLoginForm()
 
