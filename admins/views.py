5e020fe93a9cca71c9a73fb514d05101c95ee4f8
staphw <st.apostol@mail.ru>
2021-10-08 19:46:44 +0300

    mixin

--- admins/views.py
+++ admins/views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+from geekshop.mixin import CustomDispatchMixin
 from users.models import User
 
 
@@ -13,7 +16,7 @@ def index(request):
     return render(request, 'admins/admin.html')
 
 
-class UserListView(ListView):
+class UserListView(ListView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-read.html'
     context_object_name = 'users'
@@ -24,7 +27,7 @@ class UserListView(ListView):
         return context
 
 
-class UserCreateView(CreateView):
+class UserCreateView(CreateView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-create.html'
     form_class = UserAdminRegisterForm
@@ -36,7 +39,7 @@ class UserCreateView(CreateView):
         return context
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(UpdateView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
@@ -48,7 +51,7 @@ class UserUpdateView(UpdateView):
         return context
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(DeleteView, CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-read.html'
     success_url = reverse_lazy('admins:admins_user')
