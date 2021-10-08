f7dc615450d6184a1505530e636c41ee8f5a2184
staphw <st.apostol@mail.ru>
2021-10-08 11:12:03 +0300

    delete slides

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from mainapp.models import ProductCategory, Product, Slide
+from mainapp.models import ProductCategory, Product
 
 import json, os
 
