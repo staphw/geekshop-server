024b3cb52eac6befdaa0885d1a890bc56915aac4
staphw <st.apostol@mail.ru>
2021-10-08 14:52:11 +0300

    task3, lesson6

--- baskets/urls.py
+++ baskets/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import basket_add, basket_remove
+from .views import basket_add, basket_remove, basket_edit
 
 
 app_name = 'baskets'
 urlpatterns = [
     path('add/<int:product_id>/', basket_add, name='basket_add'),
     path('remove/<int:product_id>/', basket_remove, name='basket_remove'),
+    path('edit/<int:id>/<int:quantity>/', basket_edit, name='basket_edit'),
     # path('register/', register, name='register'),
     # path('logout/', logout, name='logout'),
     # path('profile/', profile, name='profile'),
