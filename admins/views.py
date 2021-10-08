4b8a66b4d68094edb4e391ebb1bea839e0a137a6
staphw <st.apostol@mail.ru>
2021-10-08 16:24:46 +0300

    admins templates, urls

--- admins/views.py
+++ admins/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+def index(request):
+    return render(request, 'admins/admin.html')
+
+
+class UserListView(ListView):
+    pass
+
+
+class UserCreateView(CreateView):
+    pass
+
+
+class UserUpdateView(UpdateView):
+    pass
+
+
+class UserDeleteView(DeleteView):
+    pass
