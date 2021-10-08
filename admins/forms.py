a6790f26f4cca5a75379cb1c27b640bf46b7e35a
staphw <st.apostol@mail.ru>
2021-10-08 17:43:22 +0300

    template admin-users-create

--- admins/forms.py
+++ admins/forms.py
@@ -1,3 +1,21 @@
 from django import forms
 
+from users.forms import UserRegisterForm
+from users.models import User
 
+
+class UserAdminRegisterForm(UserRegisterForm):
+
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name == 'image':
+                field.widget.attrs['class'] = 'form-control'
+            else:
+                field.widget.attrs['class'] = 'form-control py-4'
--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from admins.forms import UserAdminRegisterForm
+from users.models import User
 
 
 def index(request):
@@ -9,11 +13,26 @@ def index(request):
 
 
 class UserListView(ListView):
-    pass
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    context_object_name = 'users'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titel'] = 'admin | users'
+        return context
 
 
 class UserCreateView(CreateView):
-    pass
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titel'] = 'admin | create user'
+        return context
 
 
 class UserUpdateView(UpdateView):
